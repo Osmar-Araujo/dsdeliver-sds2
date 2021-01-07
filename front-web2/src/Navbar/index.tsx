5df6da3fa98c17907ec802f18e4928a8073749af
Osmar-Araujo <araujooa@gmail.com>
2021-01-07 16:34:06 -0300

    Add Navbar

---
+++ front-web2/src/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+function Navbar(){
+    return(
+        <nav className="main-navbar">
+            <a href="home" className="logo-text">DS Delivery</a>
+        </nav>
+    )
+}
+export default Navbar;
