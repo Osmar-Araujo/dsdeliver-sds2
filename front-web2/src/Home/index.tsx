a2b695c9e66acee6f422a495e014ab53e9d47361
Osmar-Araujo <araujooa@gmail.com>
2021-01-07 17:25:28 -0300

    Add Home Page

---
+++ front-web2/src/Home/index.tsx
@@ -0,0 +1,28 @@
+import './styles.css';
+import { ReactComponent as MainImage} from './Imagem.svg'
+
+function Home(){
+    return(
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-action">
+                    <h1 className="home-title">
+                        Faça seu pedido <br/>que entragamos <br/> entregamos pra você!!!!
+                    </h1>
+                    <h3 className="home-subtitle">
+                        Escolha o seu pedido e em poucos minutos<br/>
+                        levamos na sua porta
+                    </h3>
+                    <a href= "orders" className="hpme-btn-order">
+                        FAZER PEDIDO
+                    </a>
+                </div>
+                <div className="home-image">
+                    <MainImage />
+                </div>
+            </div>
+
+        </div>
+    )
+}
+export default Home;
--- front-web2/src/Navbar/index.tsx
+++ front-web2/src/Navbar/index.tsx
@@ -1,7 +1,9 @@
 import './styles.css';
+import { ReactComponent as Logo } from './Group.svg'
 function Navbar(){
     return(
         <nav className="main-navbar">
+             <Logo />
             <a href="home" className="logo-text">DS Delivery</a>
         </nav>
     )
