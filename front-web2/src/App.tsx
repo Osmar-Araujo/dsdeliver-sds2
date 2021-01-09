94ce752ddbd0cdf2d9b1be82a2f372ba4b81a5d6
Osmar-Araujo <araujooa@gmail.com>
2021-01-09 17:12:02 -0300

    Finish front-web

--- front-web2/src/App.tsx
+++ front-web2/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import React from 'react';
 import './App.css';
 
@@ -6,7 +8,10 @@ import Routes from './Routes';
 
 function App() {
   return (
-      <Routes />
+      <>
+        <Routes />
+        <ToastContainer />
+      </>
   );
 }
 export default App;
